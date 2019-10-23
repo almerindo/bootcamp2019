2d2f41c1986d1dd8597494fc2bfba0ee2a8bd461
Almerindo Rehem <almerindo@almerindo-tux>
2019-10-22 21:34:21 -0300

    Modulo 02

--- modulo02/gobarber/src/routes.js
+++ modulo02/gobarber/src/routes.js
@@ -1,7 +1,17 @@
 import { Router } from 'express';
 
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+import authMiddleware from './app/middleware/auth';
+
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Hellor OMINISTACK' }));
+routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
+
+// Este middleare global só funciona para as rotas que estiverem a baixo dele.
+routes.use(authMiddleware);
+
+routes.put('/users', UserController.update);
 
 export default routes;
